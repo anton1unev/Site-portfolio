cdf92c1d501c14bac5aa28b7ae745e2de3b9e735
Anton <lunev.anton1685@gmail.com>
2021-10-01 13:52:52 +0300

    add fio

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,12 @@
-function obrab () {
-    alert ("Обрабочик событий работает!");
+function obrab() {
+
+var textToSave = document.getElementById("fio").value;
+
+var hiddenElement = document.createElement('a');
+
+hiddenElement.href = 'data:attachment/text,' + encodeURI(textToSave);
+hiddenElement.target = '_blank';
+hiddenElement.download = 'myFile.txt';
+hiddenElement.click();
+
 }
