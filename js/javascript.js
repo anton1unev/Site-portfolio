24f0f6ce9af25e99dba06f429a3b52d3eb5deda4
Anton <lunev.anton1685@gmail.com>
2021-10-08 18:24:41 +0300

    add alert from input

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,4 @@
-function obrab() {
+/*function obrab() {
 
 var textToSave = document.getElementById("fio").value;
 
@@ -6,7 +6,26 @@ var hiddenElement = document.createElement('a');
 
 hiddenElement.href = 'data:attachment/text,' + encodeURI(textToSave);
 hiddenElement.target = '_blank';
-hiddenElement.download = 'myFile.txt';
-hiddenElement.click();
+hiddenElement.download = 'fields forms.txt';
+hiddenElement.click(); 
 
+} */
+
+/*obrab.addEventListener = function() {
+    
+    var textToSave = document.getElementById("fio").value;
+
+var hiddenElement = document.createElement('a');
+
+hiddenElement.href = 'data:attachment/text,' + encodeURI(textToSave);
+hiddenElement.target = '_blank';
+hiddenElement.download = 'fields forms.txt';
+hiddenElement.click(); 
+
+}
+*/
+
+function obrab() {
+    alert("Приветствую Вас, " + document.getElementById("fio").value);
+    alert("Ваш Email " + document.getElementById("email").value);
 }
